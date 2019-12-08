02573e7c25bf27b0b0492edfea5a4cd6b447beda
emito <aigemito.2006@gmail.com>
2019-12-08 00:19:18 -0600

    Initial commit

---
+++ app/src/main/java/com/emito/curriculumvitae/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.emito.curriculumvitae
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+        var adapter=ViewPagerAdapter(supportFragmentManager)
+        adapter.addFragment(HomeFragment(),"Home")
+        adapter.addFragment(ContactFragment(),"Contact")
+        adapter.addFragment(WorkExperienceFragment(),"Work Experience")
+        adapter.addFragment(AboutMeFragment(),"About Me")
+        viewPager.adapter=adapter
+        tabs.setupWithViewPager(viewPager)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
